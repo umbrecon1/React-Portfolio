06c8a4a36c9103235d0359231aaa547e19f8fa46
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-09-02 11:28:11 -0700

    cleande up code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -66,7 +66,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
+                  <img src={headerImg} class="headerImage" alt="Header Img"/>
                 </div>}
             </TrackVisibility>
           </Col>
